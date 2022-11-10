d81400954601a1b17c2e5c5c7ce3501bc77476ec
Godfred Doe <godfredoe9@gmail.com>
2022-11-10 20:26:50 +0000

    search engine for finding matching images from a set of images

---
+++ search.py
@@ -0,0 +1,46 @@
+"""
+USAGE 
+[Windows]
+python search.py --index index.csv --query queries/108100.png --result-path dataset
+
+[Linux/macOS]
+python3 search.py --index index.csv --query queries/108100.png --result-path dataset
+"""
+
+from scripts.colordescriptor import ColorDescriptor
+from scripts.searcher import Searcher
+import argparse 
+import cv2 
+
+
+# construct the argument parser and parse the arguments 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--index", required=True, help="path to where the computed index has been stored")
+ap.add_argument("-q", "--query", required=True, help="path to the query image")
+ap.add_argument("-r", "--result-path", required=True, help="path to the result")
+args = vars(ap.parse_args())
+
+# initialize the image descriptor 
+cd = ColorDescriptor((8, 12, 3))
+
+
+# load the query image and describe it 
+query = cv2.imread(args["query"])
+features = cd.describe(query)
+
+# perform the search 
+searcher = Searcher(args["index"])
+results = searcher.search(features)
+
+
+# display the query 
+cv2.imshow("Query", query)
+
+# loop over the results 
+for (score, resultID) in results:
+    # load the result image and display it 
+    result = cv2.imread(resultID)
+    print(resultID)
+    cv2.imshow("Result", result)
+    cv2.waitKey(0)
+
